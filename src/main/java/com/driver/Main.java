f81dabe39499602c461b31f627c46bb5555b8912
saifuddin khan <109955734+khansaif1023@users.noreply.github.com>
2024-02-20 11:34:31 +0530

    some change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args){
         Product p=new Product();
 
-        int ans=product(10,20);
-        int ans1=product(1,2,3);
-        double ans2=product(1.0,2.0);
+        int ans=p.product(10,20);
+        int ans1=p.product(1,2,3);
+        double ans2=p.product(1.0,2.0);
 
         System.out.println(ans+" "+ans1+" "+ans2);
     }
